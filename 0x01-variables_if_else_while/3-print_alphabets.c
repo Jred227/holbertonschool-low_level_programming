3199eb94f014f4a293e43ace66a62a9d7faec03c
Jared <4921@holbertonschool.com>
2022-05-23 15:44:36 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+/**
+ * Main - Entry point
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
