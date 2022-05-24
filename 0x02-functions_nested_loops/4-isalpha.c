eeb0e43707a5735b0e0cfa4112c413209dc3e6bc
Jared <4921@holbertonschool.com>
2022-05-24 15:15:50 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - alphabet
+ * @c: int
+ * Return: Always (0)
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+if (c >= 'Z' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
+}
