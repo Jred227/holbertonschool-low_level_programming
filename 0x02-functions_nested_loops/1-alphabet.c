cdfa55d874deb0e4e45b418e9b2af97ccac7c06a
Jared <4921@holbertonschool.com>
2022-05-24 11:17:54 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
+#include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	int x;
 
 	for (x = 'a'; x <= 'z'; x++)
-	{
+{
 		putchar(x);
-	}
-	{
+}
+{
 		putchar('\n');
-	}
+}
 return (0);
 }
