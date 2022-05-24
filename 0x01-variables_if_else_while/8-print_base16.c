56077304d6b8eeab48a165e8e4c8bdd364a4e60e
Jared <4921@holbertonschool.com>
2022-05-23 20:31:42 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,4 +5,19 @@
  * Return: Always 0 (success)
  */
 int main(void)
+{
+	int x;
 
+	for (x = '0'; x <= '9'; x++)
+{
+		putchar(x);
+}
+	for (x = 'a'; x <= 'f'; x++)
+{
+		putchar(x);
+}
+{
+		putchar('\n');
+}
+return (0);
+}
