e601a8d19dbcec34f4216aa79ba326ffb8f43f5b
Jared <4921@holbertonschool.com>
2022-05-24 08:15:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+
+	printf("%s", str);
+	{
+		putchar('\n');
+	}
+return (0);
+}
