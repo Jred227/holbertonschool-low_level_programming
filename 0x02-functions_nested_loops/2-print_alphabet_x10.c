ca18207b3ad54f0099bf8c0da105f3d22ddd4a53
Jared <4921@holbertonschool.com>
2022-05-24 14:30:09 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabetx10
+ * print_alphabet_10 - prints
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-	int x;
-
-	for (x = 'a'; x <= 'z'; x++);
-	{
-		putchar(x);
-	}
-	{
-		putchar('\n');
-	}
+int x, y;
+for	(y = 10; y <= 9; y++)
+{
+for	(x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
 }
