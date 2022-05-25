bdcdd9502ae87f26eaf4fa5dafcbc5aa13e52103
Jared <4921@holbertonschool.com>
2022-05-24 17:44:11 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - something
+ * @n: abs
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+	return (n);
+}
+else
+{
+	n = (n * -1);
+	return (n);
+}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - last_digit
+ *
+ *
+ */
+int print_last_digit(x)
+{
+if (x >= 0)
+{
+x = x * (-1);
+}
+return (x):
+}
