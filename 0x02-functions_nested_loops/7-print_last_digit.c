58d8952f2f04fb68fa4bb94b77ff7efe45ba201f
Jared <4921@holbertonschool.com>
2022-05-24 18:07:08 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
  * print_last_digit - last_digit
- *
- *
+ * @j: j is the generated number
+ * Return: Always j.
  */
-int print_last_digit(x)
+int print_last_digit(j)
 {
-if (x >= 0)
+j = j % 10;
+if (j < 0)
 {
-x = x * (-1);
+j = j * (-1);
 }
-return (x):
+_putchar(j + 48);
+return (j);
 }
