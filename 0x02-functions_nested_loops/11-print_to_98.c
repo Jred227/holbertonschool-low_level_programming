23988a6d076f239c0f630d83843fd3a1f19b89ac
Jared <4921@holbertonschool.com>
2022-05-25 15:07:24 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 	if (n < 98)
 		while (n < 98)
 		{
-			n++;
 			printf("%d, ", n);
+			n++;
 		}
 	else if (n > 98)
 		while (n > 98)
 		{
-			n--;
 			printf("%d, ", n);
+			n--;
 		}
 	printf("98\n");
 }
