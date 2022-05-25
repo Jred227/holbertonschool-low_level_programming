09b3b0e4a424fb5423f066cc647400e7d4b5c76e
Jared <4921@holbertonschool.com>
2022-05-25 13:10:40 -0700

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,28 +8,17 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b <= 59; b++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a != 2 && b >= 3)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			_putchar(':');
+			_putchar('0' + b / 10);
+			_putchar('0' + b % 10);
+			_putchar('\n');
 		}
 	}
 }
