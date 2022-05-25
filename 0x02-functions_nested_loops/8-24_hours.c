6b71d370f4cd6d59ca5edad81fb100d9afd8b894
Jared <4921@holbertonschool.com>
2022-05-25 09:31:51 -0700

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,12 +19,15 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if (a != 2 && b <= 3);
+					{
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
+					}
 				}
 			}
 		}
